60ebd7d98d3ae414e00c1dbf89f9bc7fa92c5bf9
Hassan Bulega <hassanbulega@gmail.com>
2023-01-09 11:51:35 +0300

    styles: add styles with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import "./App.css";
-import Button from "./components/Button";
-import Input from "./components/Input";
+import { Container } from "./components/Container";
 function App() {
   return (
     <div>
-      <Button
-        handleClick={(event,id) => {
-          console.log("Button clicked", event,id);
-        }}
-      />
-      <Input value="" handleChange={(event)=> console.log('some change',event.target)
-      }/>
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
