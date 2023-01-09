5f85dadbda9186e73504d2b5cea045995e87f098
Hassan Bulega <hassanbulega@gmail.com>
2023-01-09 10:50:37 +0300

    use events with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import "./App.css";
-import Status from "./components/Status";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Greet from "./components/Greet";
+import Button from "./components/Button";
+import Input from "./components/Input";
 function App() {
-  
   return (
     <div>
-      <Status status='success' />
-      <Heading> Placeholder text</Heading>
-      <Oscar>
-        <Heading> Oscar is a given name </Heading>
-      </Oscar>
-      <Greet name="hassan"  isLoggedIn={!false}/>
+      <Button
+        handleClick={(event,id) => {
+          console.log("Button clicked", event,id);
+        }}
+      />
+      <Input value="" handleChange={(event)=> console.log('some change',event.target)
+      }/>
     </div>
   );
 }
