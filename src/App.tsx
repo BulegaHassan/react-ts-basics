423fcf66a58be0b62bc03251558b466bbf5c5654
Hassan Bulega <hassanbulega@gmail.com>
2023-01-13 11:11:33 +0300

    use generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Profile } from "./components/auth/Profile";
 import "./App.css";
-import { Private } from "./components/auth/Private";
+import { List } from "./components/generics/List";
 function App() {
   return (
     <div>
-     <Private isLoggedIn={!true} component={Profile}/>
+      <List
+        items={["hassan", "bulega", "Abu Maryam"]}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 3, 5, 7, 9]} onClick={(item) => console.log(item)} />
+      <List
+        items={[
+          { first: "has", last: "bul" },
+          { first: "hasa", last: "bul" },
+          { first: "hasssan", last: "bul" },
+          { first: "hassanu", last: "bul" },
+          { first: "hasgotech", last: "bul" },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
